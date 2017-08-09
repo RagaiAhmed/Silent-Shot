ac638b67e08252df25c4f3e907148853067ce9db
ragaiahmed2019 <ragaiahmed@github.com>
2017-08-09 17:37:21 +0200

    animations-and-optimizations

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+	public Transform to_follow;
+
+	Vector3 diff;
+	void Start () 
+	{
+		diff = to_follow.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = to_follow.position - diff;
+		transform.rotation = Quaternion.Euler(to_follow.eulerAngles.x,to_follow.eulerAngles.y,0);
+	}
+}
