6705c7fd02b6a62fd21d740fc70b0316c3c991ae
ragaiahmed2019 <ragaiahmed@github.com>
2017-08-25 03:02:07 +0200

    added_senstivity_option_in_menu

---
+++ Assets/Scripts/Sensentivity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sensentivity : MonoBehaviour {
+
+	public float range;
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Slider> ().onValueChanged.AddListener (adjust_sens);
+	}
+
+	void adjust_sens(float done)
+	{
+		GameObject.FindGameObjectWithTag ("Player_Main").GetComponent<Head_Movement> ().Sensitivity = done * (range-1)+1;
+	}
+
+}
