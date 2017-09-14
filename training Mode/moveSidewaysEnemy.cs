cc2097da9632ef6060cb713686cfd6cad72ddc0e
yusufElnady <yusufelnady3@gmail.com>
2017-09-14 10:53:58 +0200

    training mode parts
    this commit contains the script that allows an enemy to move sideways so he can be a movng target, and the building is hollow from inside and is the mode's environment

---
+++ training Mode/moveSidewaysEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveSidewaysEnemy : MonoBehaviour {
+	public float speed = 0.25f;
+
+	void Update() {
+		transform.Translate (speed, 0f, 0f);
+	}
+	public void OnCollisionEnter(Collision col) {
+		
+		if (col.transform.name == "Cube (4)" || col.transform.name == "Cube (3)") {
+			speed = speed * -1f;
+		}
+	}
+}
